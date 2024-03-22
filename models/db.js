6b8e4a5e196376a50f7cda5bb8ec728a185153c5
Daniel Mendoza <daniel.mendozateixeira@gmail.com>
2024-03-21 22:28:49 -0300

    Update db.js

--- models/db.js
+++ models/db.js
@@ -1,5 +1,7 @@
 const Sequelize = require('sequelize');
 
+//teste
+/*
 const sequelize = new Sequelize('controle_estoque', 'root', 'root', {
     host: "127.0.0.1",
     dialect: "mysql",
@@ -7,6 +9,16 @@ const sequelize = new Sequelize('controle_estoque', 'root', 'root', {
       // Desativar a pluralização global
       freezeTableName: true
   }
+});
+*/
+//producao
+const sequelize = new Sequelize('sql10693318', 'sql10693318', 'rQl6agvMWu', {
+  host: "sql10.freemysqlhosting.net",
+  dialect: "mysql",
+  define: {
+    // Desativar a pluralização global
+    freezeTableName: true
+}
 });
 
 module.exports = {
